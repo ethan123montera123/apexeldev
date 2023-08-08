3405f0b0a9a215eec14435c3cb72814729d6656d
Ethan <128592926+ethan123montera123@users.noreply.github.com>
2023-08-08 22:02:09 +0800

    Delete controllers directory

--- controllers/fb-admin.js
+++
@@ -1,8 +0,0 @@
-const admin = require('firebase-admin')
-const serviceAccount = require('../apexel-development-firebase-adminsdk-81x2v-62264fd63a.json')
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-})
-
-module.exports = admin
